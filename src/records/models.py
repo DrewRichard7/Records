80fd0f87c777a909b884a324c148f0b96fd51c49
ham-munculus <242454600+ham-munculus@users.noreply.github.com>
2026-06-05 17:05:44 -0700

    doing a rewrite from scratch

--- src/records/models.py
+++ src/records/models.py
@@ -1,13 +1,15 @@
+"""Create sql model for schema/tables"""
+
 from datetime import datetime, timezone
 
 from sqlmodel import Field, Relationship, SQLModel
 
-
 def utc_now() -> datetime:
+    """define timezone"""
     return datetime.now(timezone.utc)
 
 
-# Create the SQLModel Model for the Category Table
+# Create the SQLModel Model for the "Category" table
 class Category(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True, unique=True)
@@ -15,15 +17,14 @@ class Category(SQLModel, table=True):
 
     entries: list["Entry"] = Relationship(back_populates="category")
 
-
-# Create the SQLModel Model for the Entry Table
+# Create the SQLModel Model for the "Entry" Table
 class Entry(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     category_name: str = Field(foreign_key="category.name", index=True)
     title: str = Field(index=True)
     creator: str = Field(default="", index=True)
     notes: str = ""
-    tags: str = Field(default="", index=True)
+    tags: str | int = Field(default="", index=True)
     created_at: datetime = Field(default_factory=utc_now, index=True)
     updated_at: datetime = Field(default_factory=utc_now)
 
